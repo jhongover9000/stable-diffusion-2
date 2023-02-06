c383618f522f1f82cd6e947b2b1cd4de7fac5c00
Joseph Hong <jhh508@nyu.edu>
2023-02-06 18:54:49 +0400

    Update calculate_times.py

--- scripts/calculate_times.py
+++ scripts/calculate_times.py
@@ -28,6 +28,7 @@ count_70 = 0
 for subdir, dirs, files in os.walk(mainDir):
     for file in files:
         if str(file) == "log.txt":
+            print(file)
             readFile = open(os.path.join(subdir, file), "r")
             # skip prompt
             readFile.readline()
@@ -35,7 +36,7 @@ for subdir, dirs, files in os.walk(mainDir):
             for i in range(3):
                 line = readFile.readline()
                 line = line.strip().split(",")
-                print(line)
+                # print(line)
                 try:
                     timeVal = float(line[1])
                 except:
@@ -48,14 +49,14 @@ for subdir, dirs, files in os.walk(mainDir):
                     if(timeVal < min_30):
                         min_30 = timeVal
                     if(timeVal > max_30):
-                        min_30 = timeVal
+                        max_30 = timeVal
                     count_30 += 1
                 elif i == 1:
                     sum_50 += timeVal
                     if(timeVal < min_50):
                         min_50 = timeVal
                     if(timeVal > max_50):
-                        min_50 = timeVal
+                        max_50 = timeVal
                     count_50 +=1
 
                 elif i == 2:
@@ -63,12 +64,12 @@ for subdir, dirs, files in os.walk(mainDir):
                     if(timeVal < min_70):
                         min_70 = timeVal
                     if(timeVal > max_70):
-                        min_70 = timeVal
+                        max_70 = timeVal
                     count_70 += 1
                 
-                print("Min 30: " + str(min_30) + "  Max 30: " + str(max_30) + " Total 30: " + str(sum_30) + " for " + str(count_30) + "images"+ "\n")
-                print("Min 50: " + str(min_50) + "  Max 50: " + str(max_50) + " Total 50: " + str(sum_50) + " for " + str(count_50) + "images"+  "\n")
-                print("Min 70: " + str(min_70) + "  Max 70: " + str(max_70) + " Total 70: " + str(sum_70) + " for " + str(count_70) + "images"+  "\n")
+                # print("Min 30: " + str(min_30) + "  Max 30: " + str(max_30) + " Total 30: " + str(sum_30) + " for " + str(count_30) + "images"+ "\n")
+                # print("Min 50: " + str(min_50) + "  Max 50: " + str(max_50) + " Total 50: " + str(sum_50) + " for " + str(count_50) + "images"+  "\n")
+                # print("Min 70: " + str(min_70) + "  Max 70: " + str(max_70) + " Total 70: " + str(sum_70) + " for " + str(count_70) + "images"+  "\n")
             readFile.close()
 
 print("Done." + "\n")
