627c369a2b250de3fbb14cd5f9f5721cd1cfcd78
jhongover9000 <57396665+jhongover9000@users.noreply.github.com>
2023-02-07 23:20:45 +0400

    Update cdf_plot.py

--- scripts/cdf_plot.py
+++ scripts/cdf_plot.py
@@ -21,9 +21,9 @@ inputFile = open(inputName, 'r')
 
 for line in inputFile:
     line = line.strip().split(",")
-    data30.append(line[0])
-    data50.append(line[1])
-    data70.append(line[2])
+    data30.append(float(line[0]))
+    data50.append(float(line[1]))
+    data70.append(float(line[2]))
 
 data30_cdf = np.sort(data30)
 data50_cdf = np.sort(data50)
@@ -32,10 +32,10 @@ data70_cdf = np.sort(data70)
 # data50_sd = calculate_standard_deviation(data50)
 # data70_sd = calculate_standard_deviation(data70)
 p = 1. * np.arange(len(data30)) / (len(data30) - 1)
-print(data30_cdf)
+# print(data30_cdf)
 
-plt.plot(data30_cdf, p, 'ro')
-plt.plot(data50_cdf, p, 'go', label = f'CDF of Inference Time at 50 Steps')
+plt.plot(data30_cdf, p, 'ro-', label = f'CDF of Inference Time at 50 Steps')
+plt.plot(data50_cdf, p, 'go+', label = f'CDF of Inference Time at 50 Steps')
 plt.plot(data70_cdf, p, 'bo', label = f'CDF of Inference Time at 70 Steps')
 
 fig = plt.figure()
