93a2c0501b52a43e40f59fd6097aba2746f569be
Joseph Hong <jhh508@nyu.edu>
2023-04-03 00:42:16 +0400

    Update promptLabeler.py

--- prompt-labeling/promptLabeler.py
+++ prompt-labeling/promptLabeler.py
@@ -4,7 +4,7 @@ import argparse
 import sys
 import time
 import csv
-openai.api_key = os.getenv("OPENAI_API_KEY=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")
+openai.api_key = os.getenv("OPENAI_API_KEY=key")
 
 readFile = open(sys.argv[1],"r")
 
@@ -45,7 +45,7 @@ readFile = open(sys.argv[1],"r")
 
 # GET LABELS
 promptStarter = "Label the prompt as one among the following: food, landscape, object, animal, celebrity, person/people, sports, text. Do not use any other words. Prompt: "
-openai.api_key = "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"
+openai.api_key = "key"
 # ended at 483 + 171
 
 outFile = open("label_list_new.txt", "w")
